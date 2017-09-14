ecccc7a0ac0eb1011b3a7ba04163be30627b1f61
Guest User <Guest@epicodus-12.local>
2017-09-14 06:42:11 -0700

    added if else statement for being exactly 18

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,10 @@ $(document).ready(function() {
 
   var age = parseInt(prompt("How old are you?"));
 
-  if (age >= 18) {
+  if (age > 18) {
+    $('#voteLinks').show();
+  } else if (age === 18) {
+    alert("congratulations you're 18...");
     $('#voteLinks').show();
   } else {
     $('#dontVote').show();
